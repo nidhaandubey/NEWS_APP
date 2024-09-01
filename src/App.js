8e3a267065fbcb6e5ac4bd316163b3477020b626
nidhaandubey <125175143+nidhaandubey@users.noreply.github.com>
2024-09-01 17:16:08 +0530

    basic structure is done

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Navbar from './Components/Navbar';
+import React, { Component } from 'react'
+import News from './Components/News';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar/>
+        <News pagesize={5}/>
+      </div>
+    )
+  }
+}
+
